73a5cffb390c23c0513b0bb8561eeca248eec570
DoglasRocha <doglas_rocha@protonmail.ch>
2022-03-13 17:32:10 -0300

    add consts and lets to search bar script

--- app/static/js/search-bar.js
+++ app/static/js/search-bar.js
@@ -1,19 +1,18 @@
-searchBar = document.querySelector('#search-bar');
-datalist = document.querySelector('#places');
-
+const searchBar = document.querySelector('#search-bar');
+const datalist = document.querySelector('#places');
 
 searchBar.addEventListener('keyup', () => {
 
-    value = searchBar.value;
+    let searchValue = searchBar.value;
 
-    request = fetch(`/search-bar?q=${value}`);
+    let request = fetch(`/search-bar?q=${searchValue}`);
     request
         .then(res => res.json())
         .then(res => {
 
             datalist.innerHTML = '';
 
-            for (name of res) 
+            for (let name of res) 
             {
                 datalist.innerHTML += `<option value="${name}">`;
             };
